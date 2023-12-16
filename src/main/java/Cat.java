0c7e439cb6ec5f964acd731ef3d0f534c62a6f4b
tarranis <danupwf@gmail.com>
2023-12-16 21:53:14 +0300

    a

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String voice;
+
+    public void setVoice(String voice) {
+        this.voice = voice;
+    }
+
+    public String getVoice() {
+        return voice;
+    }
+}
