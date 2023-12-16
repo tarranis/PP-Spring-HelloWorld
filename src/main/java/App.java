3ed6f9f5293907721a443294535f8e296bb17b95
tarranis <danupwf@gmail.com>
2023-12-16 21:38:31 +0300

    a

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        Cat catbean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catbean.getVoice());
+        Cat catbean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catbean1.getVoice());
+        System.out.println(bean == bean1);
+        System.out.println(catbean == catbean1);
     }
 }
